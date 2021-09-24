d41e0c28f9e888579d237b6b94c94dc200ba95c5
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2021-09-24 11:46:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 'use strict'
 
-// let jugadores=parseInt(prompt("Introduce el número máximo de jugadores de tu videojuego"));
-// let portada=prompt("Introduce la URL de la portada de tu videojuego");
-// let nombre=prompt("Introduce el nombre de tu videojuego");
-// let descripcion=prompt("Introduce la descripción de tu videojuego");
-// let categoria=prompt("Introduce la categoría de tu videojuego");
-// let precio=parseFloat(prompt("Introduce el precio de tu videojuego"));
-// let enlace=prompt("Introduce el enlace de la página oficial de tu videojuego");
-// let edad=confirm("¿Tu videojuego es para mayores de 18 años?");
-// let date=prompt("Introduce la fecha de lanzamiento de tu videojuego (YYYY-MM-DD)");
-// let fecha=date.split("-");
-
-let jugadores=1;
-let portada="https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_download_software_1/H2x1_NSwitchDS_Blasphemous.jpg";
-let nombre="Blasphemous";
-let descripcion="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam vel exercitationem architecto fugit perspiciatis quibusdam ab tenetur repellendus enim officia, recusandae corrupti, ex iste labore. Omnis facilis eligendi, ab exercitationem odit consectetur vel alias nam tempora cum dicta, earum molestiae dolores nisi laboriosam dolore. Quaerat sed dolorum assumenda unde est minus quae amet vitae eius ipsum numquam aliquid temporibus, asperiores, quisquam, autem saepe impedit optio at voluptatem aperiam id natus excepturi cupiditate? Totam cupiditate, accusantium dolore sequi enim repellat, inventore labore aut maxime exercitationem quam ad mollitia iste distinctio? Soluta blanditiis ipsam molestiae eius? Consequatur, laudantium blanditiis. Adipisci, omnis neque.";
-let categoria="Indie";
-let precio=12.55;
-let enlace="https://store.steampowered.com/app/774361/Blasphemous/";
-let edad="true";
-let date="2021-02-10";
+let jugadores=parseInt(prompt("Introduce el número máximo de jugadores de tu videojuego"));
+let portada=prompt("Introduce la URL de la portada de tu videojuego");
+let nombre=prompt("Introduce el nombre de tu videojuego");
+let descripcion=prompt("Introduce la descripción de tu videojuego");
+let categoria=prompt("Introduce la categoría de tu videojuego");
+let precio=parseFloat(prompt("Introduce el precio de tu videojuego"));
+let enlace=prompt("Introduce el enlace de la página oficial de tu videojuego");
+let edad=confirm("¿Tu videojuego es para mayores de 18 años?");
+let date=prompt("Introduce la fecha de lanzamiento de tu videojuego (YYYY-MM-DD)");
 let fecha=date.split("-");
 
 document.write(`
